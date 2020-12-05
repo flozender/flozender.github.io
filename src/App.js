94ba9b914b8e5816b5abd2b2689cd9b0b050c24b
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-12-05 22:29:08 +0530

    design homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Text, Flex } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex height="100%" ml="20vw" mt="40vh" flexDirection="column">
+      <Text fontSize="6xl">Hi, I'm Tayeeb Hasan.</Text>
+      <Text fontSize="3xl">🔬 & 💻 Computer Science Undergraduate.</Text>
+    </Flex>
   );
 }
 
